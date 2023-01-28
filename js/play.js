d765ad6f46c9c53638c7ba3785453f8b31d87081
taher2552 <stardom.taher@gmail.com>
2023-01-28 20:59:34 +0530

    changes

--- js/play.js
+++ js/play.js
@@ -5,12 +5,52 @@ const firstPlayer = document.querySelector('.first_player');
 const wordForm = document.querySelector('#word_input_form');
 const wordInput = document.querySelector('#word_input');
 const showWordArea = document.querySelector('.show_word_area');
+const fillIn = document.querySelector('.fill_in');
+const totalChar = document.querySelector('.total_char');
+const realWord = document.querySelector('.real_word');
+let wordInputValue;
 
 
-const totalRound=3;
+
+firstPlayer.innerText=localStorage.getItem("player1") || "player1";
+
+function wordInputSubmit(e){
+  e.preventDefault();
+  let contestantName = localStorage.getItem('player1')
+  if(wordInput.value==word){
+    wordInputValue="You guess the right word";
+    showWordArea.innerHTML += ` <div class="written_words green_background">
+    <p class="written_by">${contestantName}:</p><span class="word_line">${wordInputValue}</span>
+  </div>`
+  }else{
+    wordInputValue=wordInput.value;
+    showWordArea.innerHTML += ` <div class="written_words">
+    <p class="written_by">${contestantName}:</p><span class="word_line">${wordInputValue}</span>
+  </div>`
+  }
+  
 
 
-  let time=10;
+
+  wordInput.value=""
+}
+
+
+
+let word="cup";
+fillIn.innerText=""
+for(let i=0; i<word.length;i++){
+  fillIn.innerHTML+="_"+'&#32';
+}
+
+totalChar.innerText=word.length;
+fillIn.innerHTML+=totalChar.innerText;
+realWord.innerText = word;
+
+wordForm.addEventListener('submit',wordInputSubmit);
+
+
+let time=10;
 let timeCountDown = setInterval(()=>{
  timer.innerText=time--;
  if(time<9){
@@ -27,20 +67,6 @@ let timeCountDown = setInterval(()=>{
 
   }
 },1000);
-
-firstPlayer.innerText=localStorage.getItem("player1") || "player1";
-
-function wordInputSubmit(e){
-  e.preventDefault();
-  let contestantName = localStorage.getItem('player1')
-  showWordArea.innerHTML += ` <div class="written_words">
-  <p class="written_by">${contestantName}:</p><span class="word_line">${wordInput.value}</span>
-</div>`
-
-  wordInput.value=""
-}
-
-wordForm.addEventListener('submit',wordInputSubmit)
 
 
 
