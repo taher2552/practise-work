4233f4e3b10bd68efabb6d17fd4e2a9eccb3d39e
taher2552 <stardom.taher@gmail.com>
2023-01-28 19:56:46 +0530

    changes

--- js/play.js
+++ js/play.js
@@ -2,6 +2,10 @@
 const timer = document.querySelector('.timer');
 const resultDisplay = document.querySelector('.result_display');
 const firstPlayer = document.querySelector('.first_player');
+const wordForm = document.querySelector('#word_input_form');
+const wordInput = document.querySelector('#word_input');
+const showWordArea = document.querySelector('.show_word_area');
+
 
 const totalRound=3;
 
@@ -25,6 +29,18 @@ let timeCountDown = setInterval(()=>{
 },1000);
 
 firstPlayer.innerText=localStorage.getItem("player1") || "player1";
+
+function wordInputSubmit(e){
+  e.preventDefault();
+  let contestantName = localStorage.getItem('player1')
+  showWordArea.innerHTML += ` <div class="written_words">
+  <p class="written_by">${contestantName}:</p><span class="word_line">${wordInput.value}</span>
+</div>`
+
+  wordInput.value=""
+}
+
+wordForm.addEventListener('submit',wordInputSubmit)
 
 
 
