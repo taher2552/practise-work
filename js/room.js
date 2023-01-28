e63333ef8cdf8c90b5e2caea77d7bf1bc90abb29
taher2552 <stardom.taher@gmail.com>
2023-01-28 18:05:12 +0530

    changes

--- js/room.js
+++ js/room.js
@@ -1,2 +1,14 @@
+const linkSection = document.querySelector('.link')
 const hoverOnMe = document.querySelector('.hover_on_me');
 const hoverLink = document.querySelector('.hover_link');
+
+linkSection.addEventListener('mouseover', ()=>{
+    hoverLink.style.display="block";
+    hoverOnMe.style.display="none";
+    console.log("over")
+})
+linkSection.addEventListener('mouseout', ()=>{
+    hoverLink.style.display="none";
+    hoverOnMe.style.display="block";
+    console.log("over")
+})
